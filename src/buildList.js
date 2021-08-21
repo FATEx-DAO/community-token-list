267686b3f8e40d150f7d4fda3ac90931d9b7dad7
Corey <corey@dolomite.io>
2021-08-20 21:30:09 -0400

    Fixed remaining fork stuff

--- src/buildList.js
+++ src/buildList.js
@@ -5,7 +5,7 @@ const harmony_testnet = require("./tokens/harmony-testnet.json");
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Venomswap Community",
+    name: "FATExDAO Community",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -13,8 +13,8 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "https://d1xrz6ki9z98vb.cloudfront.net/venomswap/logos/venomswap-128x128.png",
-    keywords: ["venomswap", "community"],
+    logoURI: "https://fatex.io/fatex-token-logo-large.png",
+    keywords: ["fatex", "community"],
     tokens: [...harmony_mainnet, ...harmony_testnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
